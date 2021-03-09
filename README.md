1b70c7f0115c5d1ba72903519d2853f46ca8c87b
Mihkel Eidast <mihkel@play.ee>
2021-03-09 13:59:56 +0200

    create umd library

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webpack-5-module-library
+
+This is an example library built with webpack 5 to demo an issue when consumed in an app that uses webpack 4.
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    library: 'MyLibrary',
+    libraryTarget: 'umd',
   },
 };
